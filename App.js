80054aa3d50e6b734cab19f51698b36778a13a7e
sebranly <sebranly.pinf@gmail.com>
2019-11-01 03:20:14 +0100

    chore: prettify things

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import SectionListBasics from "./SectionListBasics";
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <SectionListBasics letterCount={2} />
       </View>
     );
   }
@@ -16,8 +16,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
   }
 });
--- SectionListBasics.js
+++ SectionListBasics.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
-import {
-  SectionList,
-  StyleSheet,
-  Text,
-  TextInput,
-  View
-} from 'react-native';
+import React from "react";
+import { SectionList, StyleSheet, Text, TextInput, View } from "react-native";
 
-import SelectedLettersText from './SelectedLettersText';
-import { pluralize } from './helpers/stringHelper';
+import SelectedLettersText from "./SelectedLettersText";
+import { pluralize } from "./helpers/stringHelper";
 
 /*
   React-native does not support dynamic imports apparently
   See https://github.com/facebook/react-native/issues/2481#issuecomment-299074154
 */
 const DATA = {
-  letterCountIs2: require('./data/words/2-letters.json'),
-  letterCountIs3: require('./data/words/3-letters.json')
-}
+  letterCountIs2: require("./data/words/2-letters.json"),
+  letterCountIs3: require("./data/words/3-letters.json")
+};
 
-export default class SectionListBasics extends Component {
+export default class SectionListBasics extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {searchText: '', isSearch: false};
+    this.state = { searchText: "", isSearch: false };
   }
 
   render() {
     const { letterCount } = this.props;
     const { isSearch, searchText } = this.state;
 
-    const getXLetterWordsSections = (jsonObject) => {
+    const getXLetterWordsSections = jsonObject => {
+      console.log(jsonObject);
       const allMixedUpWords = isSearch
-        ? jsonObject.filter((wordObject) => wordObject.englishWord.toLowerCase().includes(searchText))
+        ? jsonObject.filter(wordObject =>
+            wordObject.englishWord.toLowerCase().includes(searchText)
+          )
         : jsonObject;
       const allSortedWords = [];
       let currentGroupOfWords = [];
       let currentLetter = 97;
 
-      allMixedUpWords.forEach((wordObject) => {
+      allMixedUpWords.forEach(wordObject => {
         const firstEnglishLetter = wordObject.englishWord[0].toLowerCase();
         if (firstEnglishLetter === String.fromCharCode(currentLetter)) {
           currentGroupOfWords.push(wordObject.englishWord);
@@ -53,13 +50,13 @@ export default class SectionListBasics extends Component {
         allSortedWords.push(currentGroupOfWords);
       }
 
-      return allSortedWords.map((groupOfWords) => {
+      return allSortedWords.map(groupOfWords => {
         console.log(groupOfWords);
         const letterSection = groupOfWords[0][0].toUpperCase();
         return {
-          title: `${letterSection} (${pluralize('word', groupOfWords.length)})`,
+          title: `${letterSection} (${pluralize("word", groupOfWords.length)})`,
           data: groupOfWords
-        }
+        };
       });
     };
 
@@ -72,38 +69,40 @@ export default class SectionListBasics extends Component {
         <Text style={styles.title}>{`${letterCount}-letter words`}</Text>
         <TextInput
           placeholder="Search for a word..."
-          onChangeText={
-            (text) => {
-              const trimmedText = text.trim();
-              if (trimmedText.length > 0) {
-                this.setState({searchText: trimmedText.toLowerCase(), isSearch: true});
-              } else {
-                this.setState({isSearch: false});
-              }
+          onChangeText={text => {
+            const trimmedText = text.trim();
+            if (trimmedText.length > 0) {
+              this.setState({
+                searchText: trimmedText.toLowerCase(),
+                isSearch: true
+              });
+            } else {
+              this.setState({ isSearch: false });
             }
-          }
+          }}
           style={styles.searchBar}
         />
         {xLetterWordsSections.length ? (
           <SectionList
             sections={xLetterWordsSections}
-            renderItem={
-              ({item}) =>
-                <SelectedLettersText
-                  letters={occurrencesLetters}
-                  style={styles.item}
-                  text={item}
-                />
-            }
-            renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+            renderItem={({ item }) => (
+              <SelectedLettersText
+                letters={occurrencesLetters}
+                style={styles.item}
+                text={item}
+              />
+            )}
+            renderSectionHeader={({ section }) => (
+              <Text style={styles.sectionHeader}>{section.title}</Text>
+            )}
             keyExtractor={(item, index) => index}
           />
-        ): (
+        ) : (
           <Text style={styles.noResult}>
             {`No word was found for your search.${
               isSearch && searchText.length > letterCount
                 ? ` This view is for ${letterCount}-letter words only.`
-                : ''
+                : ""
             }`}
           </Text>
         )}
@@ -112,15 +111,19 @@ export default class SectionListBasics extends Component {
   }
 }
 
+SectionListBasics.defaultProps = {
+  letterCount: 2
+};
+
 const styles = StyleSheet.create({
   container: {
-   flex: 1,
-   paddingTop: 22
+    flex: 1,
+    paddingTop: 22
   },
   searchBar: {
     fontSize: 16,
-    borderColor: 'black',
-    textAlign: 'left',
+    borderColor: "black",
+    textAlign: "left",
     paddingLeft: 10,
     paddingBottom: 10
   },
@@ -129,23 +132,23 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingBottom: 10,
-    textAlign: 'center'
+    textAlign: "center"
   },
   sectionHeader: {
-    color: 'white',
+    color: "white",
     paddingTop: 2,
     paddingLeft: 10,
     paddingRight: 10,
     paddingBottom: 2,
     fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'black',
+    fontWeight: "bold",
+    backgroundColor: "black"
   },
   item: {
     padding: 10,
     fontSize: 18,
-    height: 44,
-  },
+    height: 44
+  }
 });
