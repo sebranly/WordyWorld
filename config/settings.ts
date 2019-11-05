e53a5670c92b9569dbb1129465948dcddd0760e7
sebranly <sebranly.pinf@gmail.com>
2019-11-05 22:35:42 +0100

    chore: delete config file that got moved already

--- config/settings.ts
+++
@@ -1,6 +0,0 @@
-// Internal
-import { getFromEnv } from "../helpers/env";
-
-export const NODE_ENV = getFromEnv("NODE_ENV") || "development";
-export const HEADER_Y = 25;
-export const IS_TEST = NODE_ENV === "test";
