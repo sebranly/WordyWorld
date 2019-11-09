c2ba21e91d6435fb38bd4e924c4280d63c2f7f2f
sebranly <sebranly.pinf@gmail.com>
2019-11-09 01:04:26 +0100

    chore: add fallback mechanism

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,6 +1,7 @@
 // Vendor
 import * as emoji from "node-emoji";
 import * as React from "react";
+import get from "lodash/get";
 import { Text } from "react-native";
 
 export interface EmojiProps {
@@ -9,10 +10,16 @@ export interface EmojiProps {
 }
 
 const Emoji: React.FC<EmojiProps> = props => {
+  // Setup
   const { text } = props;
 
   const emojiObject = emoji.find(text);
-  const emojiIcon = emojiObject ? emojiObject.emoji : undefined;
+  const fallbackEmojiObject = emoji.search(text);
+  const fallbackEmojiIcon = get(fallbackEmojiObject, "[0].emoji", null);
+  const emojiIcon = emojiObject ? emojiObject.emoji : fallbackEmojiIcon;
+
+  // Short-circuit
+  if (!emojiIcon) return null;
 
   return <Text>{emojiIcon}</Text>;
 };
