dd6f2974360e3e5383602ccb51d6186e5e65d028
sebranly <sebranly.pinf@gmail.com>
2020-01-19 04:47:28 +0100

    chore: push a commit that breaks too many tests

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,23 @@ module.exports = {
     "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(react-native" +
+    "node_modules/(?!(jest-)?react-native" +
+      "|expo(nent)?" +
+      "|@expo(nent)?/.*" +
+      "|native-base" +
       "|native-base-shoutem-theme" +
+      "|react-clone-referenced-element" +
+      "|@react-native-community" +
       "|react-native-drawer" +
       "|react-native-easy-grid" +
       "|react-native-iphone-x-helper" +
       "|react-native-keyboard-aware-scroll-view" +
       "|react-native-vector-icons" +
-      ")/)"
+      "|react-navigation" +
+      "|@react-navigation/.*|" +
+      "|sentry-expo" +
+      "|unimodules" +
+      "|@unimodules/.*" +
+      ")/"
   ]
 };
