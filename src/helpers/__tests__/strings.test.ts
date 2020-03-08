342a419e8213a3122eca9a03dd2679b19830f892
sebranly <sebranly.pinf@gmail.com>
2020-03-07 18:44:54 -0800

    feat: add letter helper

--- src/helpers/__tests__/strings.test.ts
+++ src/helpers/__tests__/strings.test.ts
@@ -1,7 +1,32 @@
 // Internal
-import { pluralize } from "../strings";
+import { decomposeWord, pluralize } from "../strings";
 
 describe("strings helper", () => {
+  it("exports the following helpers", () => {
+    expect(typeof decomposeWord).toBe("function");
+    expect(typeof pluralize).toBe("function");
+  });
+
+  describe("decomposeWord", () => {
+    // TODO: fix any
+    let result: any;
+
+    it("returns empty object for empty string", () => {
+      result = decomposeWord("");
+      expect(result).toStrictEqual({});
+    });
+
+    it("returns object with letters", () => {
+      result = decomposeWord("word");
+      expect(result).toStrictEqual({ w: 1, o: 1, r: 1, d: 1 });
+    });
+
+    it("handles words with a letter appearing several times", () => {
+      result = decomposeWord("attack");
+      expect(result).toStrictEqual({ a: 2, t: 2, c: 1, k: 1 });
+    });
+  });
+
   describe("pluralize", () => {
     let result: string;
 
--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -1,4 +1,23 @@
+const findWordConnections = (word1: string, word2: string) => {
+  if (word1 === word2) return [];
+  // TODO:
+  // return [];
+};
+
+const decomposeWord = (word: string) => {
+  const letters = word.split("");
+  // TODO: fix any
+  const occurrences: any = {};
+
+  letters.forEach(letter => {
+    if (occurrences[letter]) occurrences[letter]++;
+    else occurrences[letter] = 1;
+  });
+
+  return occurrences;
+};
+
 const pluralize = (word: string, count: number) =>
   count === 1 ? `${count} ${word}` : `${count} ${word}s`;
 
-export { pluralize };
+export { decomposeWord, pluralize };
