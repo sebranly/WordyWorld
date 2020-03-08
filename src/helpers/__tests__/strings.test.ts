ee1f64bff1be1c918f9f33587131d51accddc8eb
sebranly <sebranly.pinf@gmail.com>
2020-03-07 18:54:36 -0800

    feat: add anagram helper

--- src/helpers/__tests__/strings.test.ts
+++ src/helpers/__tests__/strings.test.ts
@@ -1,10 +1,58 @@
 // Internal
-import { decomposeWord, pluralize } from "../strings";
+import { areAnagrams, decomposeWord, pluralize } from "../strings";
 
 describe("strings helper", () => {
   it("exports the following helpers", () => {
+    expect(typeof areAnagrams).toBe("function");
     expect(typeof decomposeWord).toBe("function");
     expect(typeof pluralize).toBe("function");
+  });
+
+  describe("areAnagrams", () => {
+    let result: boolean;
+
+    it("returns false if any string is empty", () => {
+      result = areAnagrams("", "");
+      expect(result).toBe(false);
+
+      result = areAnagrams("", "a");
+      expect(result).toBe(false);
+
+      result = areAnagrams("a", "");
+      expect(result).toBe(false);
+    });
+
+    it("returns false if same string is provided twice", () => {
+      result = areAnagrams("word", "word");
+      expect(result).toBe(false);
+    });
+
+    it("returns true if words are anagrams", () => {
+      result = areAnagrams("dog", "god");
+      expect(result).toBe(true);
+
+      result = areAnagrams("chien", "niche");
+      expect(result).toBe(true);
+    });
+
+    it("returns false if the letters are different", () => {
+      result = areAnagrams("rap", "dog");
+      expect(result).toBe(false);
+
+      result = areAnagrams("mouse", "house");
+      expect(result).toBe(false);
+
+      result = areAnagrams("word", "house");
+      expect(result).toBe(false);
+
+      // Notice how all letters of 'dog' appears in 'good'
+      result = areAnagrams("dog", "good");
+      expect(result).toBe(false);
+
+      // Notice how all letters of 'dog' appears in 'dogs'
+      result = areAnagrams("dog", "dogs");
+      expect(result).toBe(false);
+    });
   });
 
   describe("decomposeWord", () => {
--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -1,7 +1,22 @@
+// Vendor
+import isEqual from "lodash/isEqual";
+
 const findWordConnections = (word1: string, word2: string) => {
   if (word1 === word2) return [];
   // TODO:
   // return [];
+};
+
+const areAnagrams = (word1: string, word2: string) => {
+  if (!word1 || !word2) return false;
+  if (word1 === word2) return false;
+
+  const decomposition1 = decomposeWord(word1);
+  const decomposition2 = decomposeWord(word2);
+
+  const valid = isEqual(decomposition1, decomposition2);
+
+  return valid;
 };
 
 const decomposeWord = (word: string) => {
@@ -20,4 +35,4 @@ const decomposeWord = (word: string) => {
 const pluralize = (word: string, count: number) =>
   count === 1 ? `${count} ${word}` : `${count} ${word}s`;
 
-export { decomposeWord, pluralize };
+export { areAnagrams, decomposeWord, pluralize };
